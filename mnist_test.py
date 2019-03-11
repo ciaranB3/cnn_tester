2be8613926cde8a824f82c2380d3a5349b14840c
ciaranB3 <kiwi94@eircom.net>
2019-03-11 17:25:52 -0400

    update

--- mnist_test.py
+++ mnist_test.py
@@ -33,20 +33,21 @@ class LIT(torch.autograd.Function):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 20, 5, 1)
-        self.conv2 = nn.Conv2d(20, 50, 5, 1)
-        self.fc1 = nn.Linear(4*4*50, 500)
-        self.fc2 = nn.Linear(500, 10)
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 4*4*50)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
 
 class DumbNet(nn.Module):
     def __init__(self):
@@ -86,39 +87,38 @@ class LitNet(nn.Module):
     
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
-    losses = []
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
+    epoch_loss = 0.0
+    batch_loss = 0.0
+    for batch_idx, (inputs, labels) in enumerate(train_loader, 0):
+        inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
+        outputs = model(inputs)
+        loss = nn.CrossEntropyLoss(outputs, labels)
         loss.backward()
         optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            # print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-            #     epoch, batch_idx * len(data), len(train_loader.dataset),
-            #     100. * batch_idx / len(train_loader), loss.item()))
-            losses.append(loss.item())
+        batch_loss += loss.item()
+        epoch_loss += loss.item()
+        if batch_idx % args.log_interval == args.log_interval:
+            print('[{}, {}] loss: {:.6f}'.format(
+                epoch, batch_idx, loss.item()))
+            batch_loss = 0.0
     return sum(losses)/float(len(losses))
 
 def test(args, model, device, test_loader):
     model.eval()
-    test_loss = 0
     correct = 0
+    total = 0
     with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+        for (inputs, labels) in test_loader:
+            images, labels = inputs.to(device), labels.to(device)
+            outputs = model(data)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-    test_loss /= len(test_loader.dataset)
-
-    # print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    #     test_loss, correct, len(test_loader.dataset),
-    #     100. * correct / len(test_loader.dataset)))
-    return test_loss
+    print('\nTest set accuracy: {:.0f}%)\n'.format(
+        100 * correct / total ))
+    return correct / total
 
 def main():
     start = time.time()
@@ -140,7 +140,7 @@ def main():
                         help='random seed (default: 1)')
     parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                         help='how many batches to wait before logging training status')
-    parser.add_argument('--save-model', action='store_true', default=True,
+    parser.add_argument('--save-model', action='store_true', default=False,
                         help='For Saving the current Model')
     parser.add_argument('--file-name', type=str, default='test_' + str(int(start))[-3:], metavar='filename',
                         help='Name of file to store model and losses')
@@ -155,19 +155,21 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('../data', train=False, transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    
+    transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+
+    testset = datasets.CIFAR10(root='./data', train=False,
+                                           download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                             shuffle=False, num_workers=2)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     qt = args.quant_type
     if qt == 'dumb':
@@ -189,13 +191,13 @@ def main():
 
     for epoch in range(1, args.epochs + 1):
         epoch_train_loss    = train(args, model, device, train_loader, optimizer, epoch)
-        epoch_test_loss = test(args, model, device, test_loader)
+        epoch_test_accuracy = test(args, model, device, test_loader)
         losses_train[epoch-1] = epoch_train_loss 
-        losses_test[epoch-1]  = epoch_test_loss
+        accuracy_test[epoch-1]  = epoch_test_accuracy
         current_time = time.time() - start
         print('\nEpoch: {:d}'.format(epoch))
         print('Training set loss: {:.6f}'.format(epoch_train_loss))
-        print('Test set loss: {:.6f}'.format(epoch_test_loss))
+        print('Test set loss: {:.6f}'.format(epoch_test_accuracy))
         print('Time taken: {:.6f}s'.format(current_time))
 
     if (args.save_model):
@@ -204,19 +206,23 @@ def main():
         torch.save(model.state_dict(),'models/' + args.file_name+'.pt')
         if not os.path.exists('data'):
             os.mkdir('data')
-        losses = np.stack((losses_train, losses_test), axis=1)
+        losses = np.stack((losses_train, accuracy_test), axis=1)
         np.savetxt('data/' + args.file_name+'.txt', losses, delimiter=', ')
 
     fig = plt.figure()
     ax = fig.gca()
-    ax.set_title('Loss per Epoch')
-    plt.plot(losses_train)
-    plt.plot(losses_test)
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    blue_line = mpatches.Patch(color='blue', label='Training Loss')
-    orange_line = mpatches.Patch(color='orange', label='Testing Loss')
-    plt.legend(handles=[blue_line, orange_line])
+    ax.set_title('Training Loss / Testing Accuracy')
+    ax.plot(losses_train, 'b-')
+    ax.set_ylabel('Loss', color='b')
+    ax.set_xlabel('Epoch')
+
+    ax2 = ax1.twinx()
+    ax2.plot(accuracy_test, 'r-')
+    ax2.set_ylabel('Accuracy (%)', color = 'r')
+    
+    # blue_line = mpatches.Patch(color='blue', label='Training Loss')
+    # orange_line = mpatches.Patch(color='orange', label='Testing Accuracy')
+    # plt.legend(handles=[blue_line, orange_line])
     plt.show()
         
 if __name__ == '__main__':
