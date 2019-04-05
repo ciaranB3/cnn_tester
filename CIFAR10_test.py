c9660729e3fd51a1c040a8d1c43fdb39897b6c4f
ciaranB3 <kiwi94@eircom.net>
2019-04-05 14:19:34 -0400

    update

--- CIFAR10_test.py
+++ CIFAR10_test.py
@@ -18,8 +18,8 @@ class LIT(torch.autograd.Function):
         # x, alpha = ctx.saved_tensors
         # print("Forward alpha: {}".format(alpha.data))
         # print("Max input value: {}".format(x.max()))
-        if num_bits!=4:
-            print("num_bits = {}".format(num_bits))
+        # if num_bits!=4:
+            # print("num_bits = {}".format(num_bits))
         ctx.num_bits = num_bits
         ctx.save_for_backward(x, alpha)
         output = x.clamp(min=0, max=alpha.data[0])
@@ -237,7 +237,7 @@ def train(model, device, train_loader, optimizer, epoch, log_interval):
         inputs, labels = inputs.to(device), labels.to(device)
         optimizer.zero_grad()
         outputs = model(inputs)
-        criterion = nn.CrossEntropyLoss()
+        criterion = nn.CrossEntropyLoss().cuda()
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
@@ -290,15 +290,25 @@ def main():
     transform = transforms.Compose(
     [transforms.ToTensor(), transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0))])
 
-    trainset = datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                              shuffle=True, num_workers=2)
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-    testset = datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=test_batch_size,
-                                             shuffle=False, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(root='./data', train=True, transform=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, 4),
+            transforms.ToTensor(),
+            normalize,
+        ]), download=True),
+        batch_size=batch_size, shuffle=True,
+        num_workers=2, pin_memory=True)
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(root='./data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=128, shuffle=False,
+        num_workers=2, pin_memory=True)
 
     if network == 'lit':
         model = litresnet18().to(device)
@@ -327,12 +337,12 @@ def main():
             learning_rate = learning_rate/10.0
             print("\nUpdating learning rate to {}\n".format(learning_rate))
             optimizer = optim.SGD(model.parameters(), lr=learning_rate, 
-                momentum=args.momentum, weight_decay=0.0002)
+                momentum=momentum, weight_decay=0.0002)
         if epoch==120:
             learning_rate = learning_rate/10.0
             print("\nUpdating learning rate to {}\n".format(learning_rate))
             optimizer = optim.SGD(model.parameters(), lr=learning_rate, 
-                momentum=args.momentum, weight_decay=0.0002)
+                momentum=momentum, weight_decay=0.0002)
         epoch_train_loss    = train(model, device, train_loader, optimizer, epoch, log_interval)
         epoch_test_accuracy = test(model, device, test_loader)
         losses_train[epoch] = epoch_train_loss 
@@ -345,18 +355,129 @@ def main():
             # model.layer1[0].lit1.alpha.data[0].item(), model.layer1[0].lit2.alpha.data[0].item()))
         print('Time taken: {:.3f}s\n'.format(current_time))
 
-    if (args.save_model):
+    if (save_model):
         if not os.path.exists('models'):
             os.mkdir('models')
-        torch.save(model.state_dict(),'models/' + args.file_name+'.pt')
+        torch.save(model.state_dict(),'models/' + file_name+'.pt')
         if not os.path.exists('results'):
             os.mkdir('results')
         losses = np.stack((losses_train, accuracy_test), axis=1)
-        np.savetxt('results/' + args.file_name+'.txt', losses, delimiter=', ')
+        np.savetxt('results/' + file_name+'.txt', losses, delimiter=', ')
 
-    fig = plt.figure()
+    fig = plt.figure(1)
     ax = fig.gca()
-    ax.set_title('Training Loss / Testing Accuracy')
+    ax.set_title('Full Resolution ResNet-20')
+    ax.plot(losses_train, 'b-')
+    ax.set_ylabel('Loss', color='b')
+    ax.set_xlabel('Epoch')
+
+    ax2 = ax.twinx()
+    ax2.plot(accuracy_test, 'r-')
+    ax2.set_ylabel('Accuracy (%)', color = 'r')
+
+    # Training settings
+
+    batch_size = 128
+    test_batch_size = 1000
+    epochs = 200
+    learning_rate = 0.1
+    momentum = 0.9
+    weight_decay = 0.0002
+    no_cuda = False
+    seed = 1
+    log_interval = 2000
+    save_model = True 
+    file_name = "LitResNet20_CIFAR10"
+    network = "lit20"
+    bit_res = 4
+
+    use_cuda = not no_cuda and torch.cuda.is_available()
+    print(torch.cuda.is_available())
+    torch.manual_seed(seed)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    
+    transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.0, 0.0, 0.0), (1.0, 1.0, 1.0))])
+
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(root='./data', train=True, transform=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, 4),
+            transforms.ToTensor(),
+            normalize,
+        ]), download=True),
+        batch_size=batch_size, shuffle=True,
+        num_workers=2, pin_memory=True)
+
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(root='./data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=128, shuffle=False,
+        num_workers=2, pin_memory=True)
+
+    if network == 'lit':
+        model = litresnet18().to(device)
+        print("Building LIT {0} bit resnet-18".format(bit_res))
+    elif network == 'lit20':
+        model = litresnet20(bit_res=bit_res).to(device)
+        print("Building LIT {0} bit ResNet-20".format(bit_res))
+    elif network == 'full20':
+        model = resnet20().to(device)
+        print("Building full resolution ResNet-20")
+    else:
+        model = resnet18().to(device)
+        print("\nBuilding full resolution ResNet-18")
+
+    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
+
+    losses_train = np.zeros((epochs))
+    accuracy_test  = np.zeros((epochs))
+
+    print(model.parameters)
+
+    start = time.time()
+
+    for epoch in range(0, epochs):
+        if epoch==60:
+            learning_rate = learning_rate/10.0
+            print("\nUpdating learning rate to {}\n".format(learning_rate))
+            optimizer = optim.SGD(model.parameters(), lr=learning_rate, 
+                momentum=momentum, weight_decay=0.0002)
+        if epoch==120:
+            learning_rate = learning_rate/10.0
+            print("\nUpdating learning rate to {}\n".format(learning_rate))
+            optimizer = optim.SGD(model.parameters(), lr=learning_rate, 
+                momentum=momentum, weight_decay=0.0002)
+        epoch_train_loss    = train(model, device, train_loader, optimizer, epoch, log_interval)
+        epoch_test_accuracy = test(model, device, test_loader)
+        losses_train[epoch] = epoch_train_loss 
+        accuracy_test[epoch]  = epoch_test_accuracy
+        current_time = time.time() - start
+        print('\nEpoch {:d} summary'.format(epoch))
+        print('Training set average loss: {:.6f}'.format(epoch_train_loss))
+        print('Test set accuracy: {:.2f}%'.format(epoch_test_accuracy))
+        # print('Layer1 alpha1: {} Layer1 alpha2: {}'.format(
+            # model.layer1[0].lit1.alpha.data[0].item(), model.layer1[0].lit2.alpha.data[0].item()))
+        print('Time taken: {:.3f}s\n'.format(current_time))
+
+    if (save_model):
+        if not os.path.exists('models'):
+            os.mkdir('models')
+        torch.save(model.state_dict(),'models/' + file_name+'.pt')
+        if not os.path.exists('results'):
+            os.mkdir('results')
+        losses = np.stack((losses_train, accuracy_test), axis=1)
+        np.savetxt('results/' + file_name+'.txt', losses, delimiter=', ')
+
+    fig2 = plt.figure(2)
+    ax = fig2.gca()
+    ax.set_title('4-bit Activations ResNet-20')
     ax.plot(losses_train, 'b-')
     ax.set_ylabel('Loss', color='b')
     ax.set_xlabel('Epoch')
